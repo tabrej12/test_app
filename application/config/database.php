b854ad3799c0f4e09ec7d91d99837fbbf86010b0
Mohammad Tabrej <mohammad.tabrej@MattsenKumar.com>
2019-04-12 19:15:19 +0530

    db config to postgre

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => '',
 	'database' => 'transport',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
